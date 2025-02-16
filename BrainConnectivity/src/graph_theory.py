5edf0d350bfe88d201e39d82234131b85bdcba3b
mcdowellkonnor <contact@konnormcdowell.com>
2025-02-16 18:57:06 -0500

    Feature Analysis and P-Values

--- BrainConnectivity/src/graph_theory.py
+++ BrainConnectivity/src/graph_theory.py
@@ -16,28 +16,55 @@ def append_connectome_data(
         each cell is a n by m matrix representing patient timeseries
 
     Returns:
-        tuple[pd.DataFrame]: Returns the `raw_data` DataFrame with appended
+        pd.DataFrame: Returns the `raw_data` DataFrame with appended
         columns
     """
 
     raw_data["Connectome"] = None
+    for index, row in raw_data.iterrows():
+        connectome = get_connectome(row["Data"])
+        raw_data.at[index, "Connectome"] = connectome
+
+    return raw_data
+
+
+def append_gt_data(raw_data: pd.DataFrame, harmonized: bool = True) -> pd.DataFrame:
+    """Wrapper method to obtain the in-place modified data matrix containing
+    connectome information in addition to the graph theory vectors.
+
+    Args:
+        raw_data (pd.DataFrame): Dataframe containing a `Data` column where
+        each cell is a n by m matrix representing patient timeseries
+
+        harmonized (bool): Flag to use 'Harmonized' column or 'Connectome'
+        column for GT calculations
+
+    Returns:
+        pd.DataFrame: Returns the `raw_data` DataFrame with appended
+        columns
+    """
+
     raw_data["EVC"] = None
     raw_data["CLU"] = None
     raw_data["DIV"] = None
     for index, row in raw_data.iterrows():
-        print(
-            f"Row {index+1} of {raw_data.shape[0]} ({np.round((index/raw_data.shape[0])*100,2)}%)"
+        connectome = (
+            raw_data.at[index, "Harmonized"]
+            if harmonized
+            else raw_data.at[index, "Connectome"]
         )
-        connectome = get_connectome(row["Data"])
-        raw_data.at[index, "Connectome"] = connectome
 
-        evc = get_evc(connectome)
+        if isinstance(connectome, np.ndarray):
+            evc = get_evc(connectome)
+            clu = get_clu(connectome)
+            div = get_div(connectome)
+        else:
+            evc = None
+            clu = None
+            div = None
+
         raw_data.at[index, "EVC"] = evc
-
-        clu = get_clu(connectome)
         raw_data.at[index, "CLU"] = clu
-
-        div = get_div(connectome)
         raw_data.at[index, "DIV"] = div
 
     return raw_data
@@ -58,55 +85,15 @@ def get_connectome(
         float: Connectome as a z by z float
     """
     # Remove any nodes which are 0 in any subject
+    # to_remove = np.where(np.sum(timeseries, axis=0) == 0)[0]
     timeseries = np.delete(
         timeseries,
-        [
-            2,
-            3,
-            4,
-            7,
-            8,
-            10,
-            75,
-            77,
-            80,
-            81,
-            114,
-            125,
-            126,
-            134,
-            177,
-            178,
-            179,
-            183,
-            184,
-            193,
-            232,
-            241,
-            242,
-            243,
-            249,
-            260,
-            261,
-            264,
-            268,
-            272,
-            273,
-            274,
-            275,
-            276,
-            279,
-            280,
-            283,
-            288,
-            289,
-            292,
-            293,
-        ],
+        [2, 9, 4, 180, 181, 249],
         axis=1,
     )
-    removed = np.where(np.sum(timeseries, axis=0) == 0)[0]
-    timeseries = timeseries[:, np.sum(timeseries, axis=0) != 0]
+    # to_remove = np.where(np.sum(timeseries, axis=0) == 0)[0]
+    # print(to_remove)
+    # timeseries = timeseries[:, np.sum(timeseries, axis=0) != 0]
 
     # Normalize each column to have unit Euclidean length
     timeseries = timeseries / np.linalg.norm(timeseries, axis=0)
